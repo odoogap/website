8a266140f32078539cad47e0ad3fb7a8cb9cb5a8
miguel figueiredo <mfigueiredo@odoogap.com>
2018-12-14 17:06:30 +0000

    [REM] remove unused models and controllers

--- website_seo_search/__manifest__.py
+++ website_seo_search/__manifest__.py
@@ -24,12 +24,6 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
