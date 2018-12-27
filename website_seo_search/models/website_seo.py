d196d3c7c05dc960ce481799e7be44090d02769b
mfigueiredo <mfigueiredo@odoogap.com>
2018-12-27 14:31:05 +0000

    [IMP] add active_tags_header method to generate title according to active tags

--- website_seo_search/models/website_seo.py
+++ website_seo_search/models/website_seo.py
@@ -1,33 +1,41 @@
 # -*- coding: utf-8 -*-
-import re
 
-from odoo import models, fields
+from odoo import models
 
 
 class Website(models.Model):
     _inherit = 'website'
 
-    def get_meta_tags(self, active_tags=False, pager=False, main_object=False, meta_type='title'):
+    def get_meta_tags(self, active_tags=False, pager=False, main_object=False):
         tag_names = ""
+        meta_tags = {'title': '', 'meta_description': ''}
         if active_tags:
             tags = self.env['blog.tag'].browse(active_tags)
             for tag in tags:
-                tag_names += tag.name
+                tag_names += tag.name + ' '
+        if main_object and 'website_meta_title' in main_object:
+            meta_tags['title'] = main_object.website_meta_title
+            if pager and isinstance(main_object.website_meta_title, str):
+                title = main_object.website_meta_title.split('|')
+                if len(title) > 1:
+                    title = '%s %s Page %s |%s' % (title[0], tag_names, pager['page']['num'], title[1])
+                else:
+                    title = '%s %s Page %s' % (title[0], tag_names, pager['page']['num'])
+                meta_tags['title'] = title
+        if main_object and main_object.website_meta_description:
+            meta_tags['meta_description'] = main_object.website_meta_description
+            if pager and isinstance(main_object.website_meta_description, str):
+                meta_description = '%s %s | Page %s' % (main_object.website_meta_description,
+                                                        tag_names,
+                                                        pager['page']['num'])
+                meta_tags['meta_description'] = meta_description
+        return meta_tags
 
-        if meta_type == 'title':
-            if pager and main_object and 'website_meta_title' in main_object:
-                if isinstance(main_object.website_meta_title, str):
-                    title = main_object.website_meta_title.split('|')
-                    if len(title) > 1:
-                        title = '%s Page %s |%s' % (title[0], pager['page']['num'], title[1])
-                    else:
-                        title = '%s Page %s' % (title[0], pager['page']['num'])
-                    return tag_names + title
-
-        if meta_type == 'description':
-            if isinstance(main_object.website_meta_description, str):
-                if pager and main_object and main_object.website_meta_description:
-                    meta_description = '%s | Page %s' % (main_object.website_meta_description, pager['page']['num'])
-                    return tag_names + meta_description
-
-        return False
+    def active_tags_header(self, active_tags=False):
+        tag_title = ""
+        if active_tags:
+            tags = self.env['blog.tag'].browse(active_tags)
+            for tag in tags:
+                tag_title += tag.name + ','
+            tag_title = tag_title[:-1]
+        return tag_title
