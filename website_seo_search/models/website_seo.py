f93a7fbd49a9f58a5df54d98781d4b7c42c88018
mfigueiredo <mfigueiredo@odoogap.com>
2019-03-21 16:06:57 +0000

    [fix] unexisting tags

--- website_seo_search/models/website_seo.py
+++ website_seo_search/models/website_seo.py
@@ -14,9 +14,7 @@ class Website(models.Model):
         meta_tags = {'title': '', 'meta_description': ''}
         active_url = http.request.httprequest.url
         title = ""
-        print('passei aqui')
-        print(active_tags)
-        print(active_url)
+
         if active_tags:
             tags = self.env['blog.tag'].browse(active_tags)
 
@@ -28,20 +26,19 @@ class Website(models.Model):
         if len(tags) > 0:
             for tag in tags:
                 if not isinstance(tag, str):
-                    tag_names += tag.name + ' '
+                    if tag.exists():
+                        tag_names += tag.name + ' '
                 else:
                     tag_names += tag + ' '
-        print(tag_names)
+
         if main_object and 'website_meta_title' in main_object:
             title = main_object.website_meta_title
             meta_tags['title'] = main_object.website_meta_title
             if pager and isinstance(main_object.website_meta_title, str):
                 title = main_object.website_meta_title.split('|')
-                print(title)
                 if len(title) > 1:
                     title = '%s %s Page %s |%s' % (title[0], tag_names, pager['page']['num'], title[
                         1]) if main_object._name != 'blog.post' else '%s %s |%s' % (title[0], tag_names, title[1])
-                    print(title)
                 else:
                     title = '%s %s Page %s' % (title[0], tag_names, pager['page'][
                         'num']) if main_object._name != 'blog.post' else '%s %s' % (title[0], tag_names)
@@ -51,7 +48,6 @@ class Website(models.Model):
                         'num']) if main_object._name != 'blog.post' else 'OdoogapBlog %s' % (tag_names)
 
             meta_tags['title'] = title
-
 
         if main_object and main_object.website_meta_description:
             meta_tags['meta_description'] = main_object.website_meta_description
@@ -67,7 +63,8 @@ class Website(models.Model):
         if active_tags:
             tags = self.env['blog.tag'].browse(active_tags)
             for tag in tags:
-                tag_title += tag.name + ', '
+                if tag.exists():
+                    tag_title += tag.name + ', '
             tag_title = self.last_occurence_rep(tag_title[:-2], ',', ' & ')
         return tag_title
 
